5962e711efaa7efbeaefa4a3f1971cee64209ab1
Dmatrix <dhanu@gmail.com>
2023-06-04 08:30:30 +0530

    Changes to push on develop branch

--- src/test/java/test/rest/SampleClass.java
+++ src/test/java/test/rest/SampleClass.java
@@ -1,6 +1,11 @@
 package test.rest;
 
 public class SampleClass {
+	
+	public static void sample()
+	{
+		System.out.println("This method is written to push on develop branch");
+	}
 
 	public static void main(String[] args) {
 
@@ -9,6 +14,7 @@ public class SampleClass {
 		//This is change by UserX
 		
 		//This is new change by UserY
+		
 
 		
 
